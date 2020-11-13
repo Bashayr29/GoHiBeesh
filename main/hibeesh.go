76317e3c4b1cb20c9d4f67bfbe1adc300480828b
Bashayr Alabdullah <bshayr29@gmail.com>
2020-11-13 22:16:04 +0300

    update

--- main/hibeesh.go
+++ main/hibeesh.go
@@ -8,7 +8,7 @@ import (
 )
 func handler(w http.ResponseWriter, r *http.Request)  {
 	dt := time.Now()
-	fmt.Fprintf(w, "Hi Beesh, today is: ", dt.Format("01-02-2006 Mon"))
+	fmt.Fprintf(w, "Hi Beesh, today is: %s ", dt.Format("01-02-2006 Mon"))
 }
 func main() {
 	// fmt.Println("hi beesh")
