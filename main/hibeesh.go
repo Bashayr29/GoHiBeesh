383bd0b0470e72577d725d7459d49ebc19b5832d
Bashayr Alabdullah <bshayr29@gmail.com>
2020-11-13 20:06:41 +0300

    add dockerfile

--- main/hibeesh.go
+++ main/hibeesh.go
@@ -2,7 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"log"
+	"net/http"
 )
+func handler(w http.ResponseWriter, r *http.Request)  {
+	dt := time.Now()
+	fmt.Fprintf(w, "Hi Beesh, today is: ", dt.Format("01-02-2006 Mon"))
+}
 func main() {
-	fmt.Println("hi beesh");
+	// fmt.Println("hi beesh")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
