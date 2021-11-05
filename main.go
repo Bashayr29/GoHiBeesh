e5c849fc86a87315f4b1119b63db1d989ba0ab3c
Bashayr <bshayr29@gmail.com>
2021-11-06 02:15:58 +0300

    :rocket:  test for deploy
    BREAKING CHANGE: nothing to write

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	dt := time.Now()
-	fmt.Fprintf(w, "Good morning Beesh!, today is: %s ", dt.Format("01-02-2006 Mon"))
+	fmt.Fprintf(w, "Good morning Beesh, today is: %s ", dt.Format("01-02-2006 Mon"))
 }
 func main() {
 	// fmt.Println("hi beesh")
